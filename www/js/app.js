948b5d048a1bfaacf925466575010c7237f5fb96
ivanguzman6 <ivanguzman6@hotmail.com>
2015-08-13 21:32:10 -0400

    pantalla con objetos

--- www/js/app.js
+++ www/js/app.js
@@ -1,24 +1,30 @@
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'MiApp' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('MiApp', ['ionic'])
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('mi_consumo', ['ionic', 'mi_consumo.controllers', 'mi_consumo.services'])
 
-	.run(function($ionicPlatform) {
-	  $ionicPlatform.ready(function() {
-		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-		// for form inputs)
-		if(window.cordova && window.cordova.plugins.Keyboard) {
-		  cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-		}
-		if(window.StatusBar) {
-		  StatusBar.styleDefault();
-		}
-	  });
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleLightContent();
+    }
+  });
+})
 	  
-	  
-	.config(function($stateProvider, $urlRouterProvider) {
+
+.config(function($stateProvider, $urlRouterProvider) {
 
 	// Ionic uses AngularUI Router which uses the concept of states
 	// Learn more here: https://github.com/angular-ui/ui-router
@@ -28,7 +34,7 @@ angular.module('MiApp', ['ionic'])
 
 	.state ('registro_consumo', {
 		url:"/registro_consumo" ,
-		templateUrl:'paginas/registro_consumo.html', 
+		templateUrl:'template/registro_consumo.html', 
 		controller:'registro_consumoCtrl'
 	})
 
---
+++ www/js/controllers.js
@@ -0,0 +1,12 @@
+angular.module('mi_consumo.controllers', [])
+
+.controller('registro_consumoCtrl', function($scope,$state) 
+{
+	
+	
+	$scope.salvar  = function() 
+	{
+		alert("Datos Salvados");
+	};
+})
+
