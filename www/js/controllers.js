fef2903bc0696c0346547b89acc732bd6f2b40bd
ivanguzman6 <ivanguzman6@hotmail.com>
2015-08-13 22:10:41 -0400

    no funcionan los ng-model

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,11 +2,18 @@ angular.module('mi_consumo.controllers', [])
 
 .controller('registro_consumoCtrl', function($scope,$state) 
 {
-	
+	$scope.fecha_consumo="";
+	$scope.kilometraje="0";
 	
 	$scope.salvar  = function() 
 	{
-		alert("Datos Salvados");
+		//if ( === 'widescreen') 
+		//{
+		//  return '270px';
+		//} else {
+		//  return '360px';
+		//}
+		alert("Datos Salvados <"+$scope.kilometraje+">");
 	};
 })
 
