602add0ba370242d2ec71bab02e33ce5a6ce617d
ivanguzman6 <ivanguzman6@hotmail.com>
2015-08-15 18:04:05 -0400

    el precio funciona

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,12 +10,28 @@ angular.module('mi_consumo.controllers', [])
 		'precio_galon' : 0
 	  };
   
-	$scope.fecha_consumo="";
-	precio_galon=$scope.monto_consumo/$scope.galones_consumo;
+	
+	
+	
+	$scope.parseFloat = function(value)
+    {
+        return parseFloat(value);
+    };
+	
+	$scope.calcular_precio = function()
+	{
+		if($scope.consumo.monto_consumo!=0 && $scope.consumo.galones_consumo!=0)
+		{
+			$scope.consumo.precio_galon = $scope.redondear($scope.consumo.monto_consumo/$scope.consumo.galones_consumo,2);
+		}else
+		{
+			$scope.consumo.precio_galon = 0;
+		};
+	};
 	
 	$scope.salvar  = function() 
 	{
-		// if (!{{registro_consumo.$valid}}) 
+		//if (!{{registro_consumo.$valid}}) 
 		// {
 		  // alert("Error validacion: "+$scope.consumo.kilometraje+">");
 		// } 
@@ -25,5 +41,13 @@ angular.module('mi_consumo.controllers', [])
 		// };
 		alert("Datos Salvados <"+$scope.consumo.kilometraje+">");
 	};
+	
+	$scope.redondear = function (number, precision)
+	{
+		precision = Math.abs(parseInt(precision)) || 0;
+		var multiplier = Math.pow(10, precision);
+		return (Math.round(number * multiplier) / multiplier);
+	};
+
 })
 
