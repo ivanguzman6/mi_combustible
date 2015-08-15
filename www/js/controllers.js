46a464fc3f7dda2ed1f2d11a0f7039ef8bbf427a
ivanguzman6 <ivanguzman6@hotmail.com>
2015-08-15 13:04:01 -0400

    campos validados, precio sin terminar

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,20 +3,26 @@ angular.module('mi_consumo.controllers', [])
 .controller('registro_consumoCtrl', function($scope,$state) 
 {
 	$scope.consumo = {
-		'kilometraje' : 0
+		'fecha_consumo' : "",
+		'kilometraje' : 0,
+		'monto_consumo': 0,
+		'galones_consumo' : 0,
+		'precio_galon' : 0
 	  };
   
 	$scope.fecha_consumo="";
-	$scope.kilometraje="0";
+	precio_galon=$scope.monto_consumo/$scope.galones_consumo;
 	
 	$scope.salvar  = function() 
 	{
-		//if ( === 'widescreen') 
-		//{
-		//  return '270px';
-		//} else {
-		//  return '360px';
-		//}
+		// if (!{{registro_consumo.$valid}}) 
+		// {
+		  // alert("Error validacion: "+$scope.consumo.kilometraje+">");
+		// } 
+		// else 
+		// {
+		  // alert("Datos Salvados <"+$scope.consumo.kilometraje+">");
+		// };
 		alert("Datos Salvados <"+$scope.consumo.kilometraje+">");
 	};
 })
